a0402e6b60e232e2224700b9c08604d6ab78b731
luizalvesot <alvesluizotavio23@gmail.com>
2023-06-24 16:14:13 -0300

    Projeto Iniciado

---
+++ app/Http/Controllers/converteasy/ConvertController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\converteasy;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ConvertController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function getMoedaAPI()
+	{
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'GET',
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		//echo $response;
+	 
+		$obj = json_decode($response);
+
+		$valorDolar = $obj->USDBRL->ask;
+
+		$valorDolarFormated = substr($valorDolar, 0,-2);
+
+        $valorEuro = $obj->EURBRL->ask;
+
+		$valorEuroFormated = substr($valorEuro, 0,-2);
+
+		return view('converteasy.index',compact('valorDolarFormated', 'valorEuroFormated'));
+	}
+}
---
+++ resources/views/converteasy/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    
+    <div class="card m-2">
+        <div class="card-header">{{ __('Moedas mais usadas mundialmente') }}</div>    
+            <div class="card-body">
+                <div class="row justify-content-center">
+            </div>                  
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card m-3 shadow-lg border-0">  
+                    <div class="card-body">
+                    
+                        <div class="row justify-content-center">
+                            <h4 class="mb-4 text-dark">DÓLAR HOJE</h4>
+                        </div>
+                        <?php $value = 5; ?>
+                        <div class="row justify-content-center">
+                            <div class="alert alert-primary col-4 m-2 text-center shadow-lg" role="alert">
+                                <h4>Dólar Americano</h4>
+                                <h1>US$ <?php echo $value; ?></h1>
+                            </div>
+                            <div class="pt-4 m-4">
+                                <h3><i class="bi bi-arrow-right-square-fill"></i></h3>
+                            </div>
+                            <div class="alert alert-success col-4 m-2 text-center shadow-lg" role="alert">
+                                <h4>Real Brasileiro</h4>
+                                <h1>R$ {{ $valorDolarFormated * $value }}</h1>
+                            </div>
+                        </div>  
+
+                        <div class="row justify-content-center">
+                            <div class="alert alert-warning col-4 m-2 text-center shadow-lg" role="alert">
+                                <h4>Euro</h4>
+                                <h1>€ 1.00</h1>
+                            </div>
+                            <div class="pt-4 m-4">
+                                <h3><i class="bi bi-arrow-right-square-fill"></i></h3>
+                            </div>
+                            <div class="alert alert-success col-4 m-2 text-center shadow-lg" role="alert">
+                                <h4>Real Brasileiro</h4>
+                                <h1>R$ {{ $valorEuroFormated }}</h1>
+                            </div>
+                        </div>    
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
