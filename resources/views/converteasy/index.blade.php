1e77a05f20ba0eaa69e677180aeabee9bc716b1d
luizalvesot <alvesluizotavio23@gmail.com>
2023-08-08 09:45:29 -0300

    Ajustando layout dos inputs

--- resources/views/converteasy/index.blade.php
+++ resources/views/converteasy/index.blade.php
@@ -23,40 +23,53 @@
 
                 <div class="col-md-9 shadow-sm bg-white p-4">
                     <div class="row justify-content-center">
-                        <div class="p-2 m-2 shadow-lg" style="border-radius: 50%; width: 100px; height: 100px; background-image:url('https://www.infomoney.com.br/wp-content/uploads/2019/06/eua.jpg?resize=900%2C515&quality=50&strip=all'); background-size: 100% 100%;">
-                        </div>
+                        <div class="p-2 m-2 shadow-lg circle-init" style="background-image:url('https://www.infomoney.com.br/wp-content/uploads/2019/06/eua.jpg?resize=900%2C515&quality=50&strip=all'); background-size: 100% 100%;"></div>
                     </div>
                     <div class="row justify-content-center">
                         <h4 class="mb-4 text-dark">DÓLAR AMERICANO</h4>
                     </div>
                     
-
                     <?php $value = 1; ?>
 
-                    <div class="row justify-content-center mt-2">
-                        <div class="col-4 m-2 text-center" role="alert">
-                            <form action="">
-                                <input class="form-control shadow-lg form-control-lg text-info border-0" style="font-family: Tahoma; font-size: 210%" type="text" value="US$ <?php echo $value; ?> dólar">
-                            </form>
+                    <div class="container">
+                        <div class="row mt-2 justify-content-center">
+                            <div class="m-2 col-sm" role="alert">
+                                <span class="row p-2 cot-moeda-group shadow">
+                                    <span class="cot-symbol">US$</span>
+                                    <input class="cot-moeda" onClick="this.setSelectionRange(0, this.value.length)" 
+                                           type="text" value="1,00">
+                                </span>
+                            </div>
+                            <div class="mt-5 m-2">
+                                <span class="text-success font-weight-bold">
+                                    VALE
+                                </span>
+                            </div>
+                            <div class="m-2 col-sm" role="alert">
+                                <span class="row p-2 cot-moeda-group shadow">
+                                    <span class="cot-symbol">R$</span>
+                                    <input class="cot-moeda" onClick="this.setSelectionRange(0, this.value.length)" 
+                                           type="text" value="{{ $valorDolarFormated * 1 }}">
+                                </span>
+                            </div>
+                            <div class="mt-5 m-2">
+                                <span class="text-success font-weight-bold">
+                                    HOJE
+                                </span>
+                            </div>
                         </div>
+                        <div class="alert alert-danger alert-dismissible fade show mt-4 m-2" role="alert">
+                            Clique em cima do valor para alterá-lo, se necessário!
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>  
 
-                        <div class="pt-2 mt-5">
-                            <h5 class="text-success">vale</h5>
-                        </div>
-                        <div class="col-4 m-2 text-center">
-                            <form action="">
-                                <input class="form-control shadow-lg form-control-lg text-success border-0" style="font-family: Tahoma; font-size: 210%" type="text" value="R$ {{ $valorDolarFormated * $value }} reais">
-                            </form>
-                        </div>
-                        <div class="pt-2 mt-5">
-                            <h5 class="text-success">hoje</h5>
-                        </div>
-                    </div>  
-                </div>                
             </div>
         </div>
       
-        
         <!--Coluna reservada apenas para Google Adscence-->
         <div class="col-md-2 m-2">
             <div class="card border-0 shadow-sm">    
